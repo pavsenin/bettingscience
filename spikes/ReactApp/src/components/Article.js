6c370a1b61703e2cef043d4b9307554a8cfbaa67
pavsenin <pavsenin@mail.ru>
2018-12-07 19:28:22 +0300

    ReactApp v3

--- spikes/ReactApp/src/components/Article.js
+++ spikes/ReactApp/src/components/Article.js
@@ -4,30 +4,29 @@ class Article extends PureComponent {
     constructor(props) {
         super(props)
         this.state = {
-            isOpen : props.defaultOpen,
             count : 0
         }
     }
-    componentWillReceiveProps(nextProps) {
-        if(nextProps.defaultOpen !== this.props.isOpen)
-            this.setState({
-                isOpen : nextProps.defaultOpen
-            })
-    }
+    // componentWillReceiveProps(nextProps) {
+    //     if(nextProps.defaultOpen !== this.props.isOpen)
+    //         this.setState({
+    //             isOpen : nextProps.defaultOpen
+    //         })
+    // }
     componentWillUpdate() {
         console.log('---', 'will update')
     }
     render() {
-        const {article} = this.props
-        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+        const {article, isOpen, onButtonClick} = this.props
+        const body = isOpen && <section className="card-text">{article.text}</section>
         return (
             <div className="card mx-auto" style={{width:'50%'}}>
                 <div className="card-header">
                     <h2 onClick={this.incrementCount}>
                         {article.title}
                         counted {this.state.count}
-                        <button className="btn btn-primary btn-lg float-right" onClick={this.handleClick}>
-                            {this.state.isOpen ? 'close':'open'}
+                        <button className="btn btn-primary btn-lg float-right" onClick={onButtonClick}>
+                            {isOpen ? 'close':'open'}
                         </button>
                     </h2>
                 </div>
@@ -43,11 +42,6 @@ class Article extends PureComponent {
     incrementCount = () => {
         this.setState({
             count : this.state.count + 1
-        })
-    }
-    handleClick = () => {
-        this.setState({
-            isOpen : !this.state.isOpen
         })
     }
 }
--- spikes/ReactApp/src/components/ArticleList/index.js
+++ spikes/ReactApp/src/components/ArticleList/index.js
@@ -3,10 +3,13 @@ import Article from '../Article'
 import './style.css'
 
 export default class ArticleList extends PureComponent {
+    state = {
+        openArticleId: null
+    }
     render() {
         const articleElements = this.props.articles.map((article, index) =>
             <li className="article-list__li" key={article.id}>
-                <Article article={article} defaultOpen={index === 0} />
+                <Article article={article} isOpen={this.state.openArticleId === article.id} onButtonClick={this.handleClick.bind(this, article.id)}/>
             </li>
         )
         return (
@@ -14,5 +17,10 @@ export default class ArticleList extends PureComponent {
                 {articleElements}
             </ul>
         )
+    }
+    handleClick = openId => {
+        this.setState({
+            openArticleId: this.state.openArticleId === openId ? null : openId
+        })
     }
 }
