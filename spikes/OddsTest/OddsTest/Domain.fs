046d3a40e9f59dcdf7087d59615677b1e3184dbe
Pavel Avsenin <pavsenin@mail.ru>
2019-06-17 08:09:50 +0300

    Next

---
+++ spikes/OddsTest/OddsTest/Domain.fs
@@ -0,0 +1,46 @@
+﻿module Domain
+open Newtonsoft.Json
+
+type MatchScore = {
+    [<JsonProperty(PropertyName = "home")>]
+    Home : int
+    [<JsonProperty(PropertyName = "away")>]
+    Away : int
+}
+type Outcome2Odds = {
+    [<JsonProperty(PropertyName = "home")>]
+    Home : float32
+    [<JsonProperty(PropertyName = "away")>]
+    Away : float32
+}
+type Outcome3Odds = {
+    [<JsonProperty(PropertyName = "home")>]
+    Home : float32
+    [<JsonProperty(PropertyName = "draw")>]
+    Draw : float32
+    [<JsonProperty(PropertyName = "away")>]
+    Away : float32
+}
+type OutcomeOdds = X2 of Outcome2Odds | X3 of Outcome3Odds
+type MatchOdds = {
+    [<JsonProperty(PropertyName = "starting")>]
+    Starting : OutcomeOdds
+    [<JsonProperty(PropertyName = "closing")>]
+    Closing : OutcomeOdds
+}
+type MatchData = {
+    [<JsonProperty(PropertyName = "id")>]
+    ID : string
+    [<JsonProperty(PropertyName = "url")>]
+    Url : string
+    [<JsonProperty(PropertyName = "score")>]
+    Score:MatchScore
+    [<JsonProperty(PropertyName = "odds")>]
+    Odds:MatchOdds
+}
+type LeagueData = {
+    [<JsonProperty(PropertyName = "id")>]
+    ID : string
+    [<JsonProperty(PropertyName = "matches")>]
+    Matches:MatchData array
+}
--- spikes/OddsTest/OddsTest/Program.fs
+++ spikes/OddsTest/OddsTest/Program.fs
@@ -4,18 +4,15 @@ open System.Net
 open FSharp.Data
 open FSharp.Data.JsonExtensions
 open HtmlAgilityPack
-open System.Security.Policy
-open System.Net
+open Domain
+open Microsoft.FSharpLu.Json
 
 let defArg defaultValue arg = defaultArg arg defaultValue
-let (|>>) v f = v |> Option.map f
-let (||>) v f = v |> Option.bind f
+let (|>>) v f = try v |> Option.map f with | _ -> None
+let (||>) v f = try v |> Option.bind f with | _ -> None
 let (|><|) v1 v2 = match v1, v2 with | Some x1, Some x2 -> Some (x1, x2) | _ -> None
 
-let oddsDataHost = "fb.oddsportal.com"
-let matchHost = "www.oddsportal.com"
-let matchReferer = "http://www.oddsportal.com/soccer/russia/premier-league/lokomotiv-moscow-spartak-moscow-nF3P6xBd/"
-
+let oddsportalHost = "www.oddsportal.com"
 
 let fromUnixTimestamp() =
     let origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)
@@ -31,51 +28,88 @@ let fetchContent (url:string) host referer =
     client.DownloadString(url)
 
 let extractDataFromResponse dataFilePath (response:string) =
-    let start = "globals.jsonpCallback('" + dataFilePath + "', "
-    let finish = ");"
+    let start, finish = "globals.jsonpCallback('" + dataFilePath + "', ", ");"
     if response.StartsWith(start) && response.EndsWith(finish) then
-        let index = start.Length
         let length = response.Length - start.Length - finish.Length
-        Some (response.Substring(index, length))
+        Some (response.Substring(start.Length, length))
     else
         None
 
+let soccerID = "1"
+let baseballID = "6"
+let esportsID = "36"
+
+let out1x2ID = "1"
+let outHomeAwayID = "3"
+
 let pinnacleID = "18"
 
-let parseFootballMatchResponse id content =
-    let getOutcomeIds (id:JsonValue) =
-        match id with
-        | JsonValue.Array x ->
-            Some("", "", "")
-        | JsonValue.Record _ ->
-            Some(id.["0"].AsString(), id.["1"].AsString(), id.["2"].AsString())
-        | _ -> None
+let asString (value:JsonValue) = value.AsString()
+let asFloat (value:JsonValue) = float32(value.AsFloat())
+let getAttribute (node:HtmlNode) func =
+    node.Attributes |> List.ofSeq |> List.tryFind func
+
+let getOutcomes3 asFunc = function
+    | JsonValue.Array [|home; draw; away|] ->
+        Some(asFunc home, asFunc draw, asFunc away)
+    | JsonValue.Record _ as value ->
+        Some(asFunc value.["0"], asFunc value.["1"], asFunc value.["2"])
+    | _ -> None
+let getOutcomes2 asFunc = function
+    | JsonValue.Array [|home; away|] ->
+        Some(asFunc home, asFunc away)
+    | JsonValue.Record _ as value ->
+        Some(asFunc value.["0"], asFunc value.["1"])
+    | _ -> None
+let getStringOutcomes3, getFloatOutcomes3 = getOutcomes3 asString, getOutcomes3 asFloat
+let getStringOutcomes2, getFloatOutcomes2 = getOutcomes2 asString, getOutcomes2 asFloat
+
+let getStartingClosingOdds3 (historyData:JsonValue) pinnacleOdds outcomeID =
+    getStringOutcomes3 outcomeID ||> (fun (homeId, drawId, awayId) ->
+        let pinnacleHistory0, pinnacleHistory1, pinnacleHistory2 =
+            historyData.[homeId].[pinnacleID], historyData.[drawId].[pinnacleID], historyData.[awayId].[pinnacleID]
+            
+        getFloatOutcomes3 pinnacleOdds |>> (fun (homeOddsClosing, drawOddsClosing, awayOddsClosing) ->
+            let extractStartingOdds (history:JsonValue) =
+                let start = history.AsArray() |> Array.head
+                let odds = start.AsArray() |> Array.head
+                asFloat odds
+            let homeOddsStarting, drawOddsStarting, awayOddsStarting =
+                extractStartingOdds pinnacleHistory0, extractStartingOdds pinnacleHistory1, extractStartingOdds pinnacleHistory2
+            {
+                Starting = X3 { Home = homeOddsStarting; Draw = drawOddsStarting; Away = awayOddsStarting };
+                Closing = X3 { Home = homeOddsClosing; Draw = drawOddsClosing; Away = awayOddsClosing }
+            }
+        )
+    )
+let getStartingClosingOdds2 (historyData:JsonValue) pinnacleOdds outcomeID =
+    getStringOutcomes2 outcomeID ||> (fun (homeId, awayId) ->
+        let pinnacleHistory0, pinnacleHistory1 =
+            historyData.[homeId].[pinnacleID], historyData.[awayId].[pinnacleID]
+            
+        getFloatOutcomes2 pinnacleOdds |>> (fun (homeOddsClosing, awayOddsClosing) ->
+            let extractStartingOdds (history:JsonValue) =
+                let start = history.AsArray() |> Array.head
+                let odds = start.AsArray() |> Array.head
+                asFloat odds
+            let homeOddsStarting, awayOddsStarting =
+                extractStartingOdds pinnacleHistory0, extractStartingOdds pinnacleHistory1
+            {
+                Starting = X2 { Home = homeOddsStarting; Away = awayOddsStarting };
+                Closing = X2 { Home = homeOddsClosing; Away = awayOddsClosing }
+            }
+        )
+    )
+
+let parseFootballMatchResponse (outID, hzID) id content =
     let json = extractDataFromResponse id content |> Option.map JsonValue.Parse
-    json |> Option.map (fun value ->
-        let oddsData = value?d?oddsdata?back.["E-1-2-0-0-0"]
-        let historyData = value?d?history?back
-        let odds, outcomeID = oddsData?odds, oddsData?OutcomeID
-        let 
-        let (homeId, drawId, awayId) = getOutcomeIds outcomeID
-        let history0, history1, history2 = historyData.[homeId], historyData.[drawId], historyData.[awayId]
-
-        let pinnacleOdds = odds.[pinnacleID]
-        let pinnacleHistory0 = history0.[pinnacleID]
-        let pinnacleHistory1 = history1.[pinnacleID]
-        let pinnacleHistory2 = history2.[pinnacleID]
-
-        let homeOddsClosing, drawOddsClosing, awayOddsClosing = pinnacleOdds.["0"].AsFloat(), pinnacleOdds.["1"].AsFloat(), pinnacleOdds.["2"].AsFloat()
-
-        let extractStartingOdds (history:JsonValue) =
-            let start = history.AsArray() |> Array.head
-            let odds = start.AsArray() |> Array.head
-            odds.AsFloat()
-        let homeOddsStarting, drawOddsStarting, awayOddsStarting =
-            extractStartingOdds pinnacleHistory0,
-            extractStartingOdds pinnacleHistory1,
-            extractStartingOdds pinnacleHistory2
-        
-        (homeOddsStarting, drawOddsStarting, awayOddsStarting), (homeOddsClosing, drawOddsClosing, awayOddsClosing)
+    json ||> (fun value ->
+        let oddsData = value?d?oddsdata?back.["E-" + outID + "-" + hzID + "-0-0-0"]
+        let historyData, pinnacleOdds, outcomeID = value?d?history?back, oddsData?odds.[pinnacleID], oddsData?OutcomeID
+        match outID with
+        | "1" -> getStartingClosingOdds3 historyData pinnacleOdds outcomeID
+        | "3" -> getStartingClosingOdds2 historyData pinnacleOdds outcomeID
+        | _ -> None
     )
 
 let parseMainMatchPage url =
@@ -95,7 +129,7 @@ let parseMainMatchPage url =
                 let start = hashStart + hashStartText.Length
                 let xhash = restText.Substring(start, hashEnd - start)
                 WebUtility.UrlDecode(xhash) |> Some
-    let html = fetchContent url matchHost url
+    let html = fetchContent url oddsportalHost url
     let document = HtmlDocument()
     document.LoadHtml(html)
     let xhash = document.DocumentNode.SelectNodes("/html/body/script") |> List.ofSeq |> Seq.tryPick (fun script -> extractXHashKey script.InnerText)
@@ -110,45 +144,112 @@ let parseMainMatchPage url =
         )
     xhash |><| score
 
-let getAttribute (node:HtmlNode) func =
-    node.Attributes |> List.ofSeq |> List.tryFind func
+let extractMatches (document:HtmlDocument) =
+    document.DocumentNode.SelectNodes("/table/tbody/tr") |> List.ofSeq |> List.choose (fun tr ->
+        let xeid = getAttribute tr (fun attr -> attr.Name = "xeid") |>> (fun attr -> attr.Value)
+        let matchUrl = tr.ChildNodes |> List.ofSeq |> List.tryPick (fun child ->
+            getAttribute child (fun attr -> attr.Name = "class" && attr.Value = "name table-participant") |>> (fun _ ->
+                child.ChildNodes |> List.ofSeq |> List.tryFind (fun c -> c.Name = "a") |>> (fun a ->
+                    getAttribute a (fun attr -> attr.Name = "href") |>> (fun attr -> attr.Value) |> defArg ""
+                ) |> defArg ""
+            )
+        )
+        xeid |><| matchUrl
+    )
+
+let extractOdds (sportID, outID, hzID) (matchID, matchRelativeUrl) =
+    let matchUrl = "http://www.oddsportal.com/" + matchRelativeUrl
+    let xhash = parseMainMatchPage matchUrl
+    xhash ||> (fun (hash, (score1, score2)) ->
+        let matchData = "/feed/match/1-" + sportID + "-" + matchID + "-" + outID + "-" + hzID + "-" + hash + ".dat"
+        let matchDataUrl = "https://fb.oddsportal.com" + matchData + "?_=" + fromUnixTimestamp()
+        let matchContent = fetchContent matchDataUrl "fb.oddsportal.com" "https://www.oddsportal.com/"
+        let pinnacleOdds = parseFootballMatchResponse (outID, hzID) matchData matchContent
+        pinnacleOdds |>> (fun odds -> { ID = matchID; Url = matchUrl; Score = { Home = score1; Away = score2 }; Odds = odds })
+    )
+
+type MatchResult = Home | Draw | Away
+let getMatchResult ({ Home = home; Away = away }:MatchScore) =
+    if home > away then Home else if home = away then Draw else Away
+
+let getProbabilities = function
+    | X3 { Home = home; Draw = draw; Away = away } ->
+        let homeProb, drawProb, awayProb = 1.f / home, 1.f / draw, 1.f / away
+        let sumProb = homeProb + drawProb + awayProb
+        homeProb / sumProb
+    | X2 { Home = home; Away = away } ->
+        let homeProb, awayProb = 1.f / home, 1.f / away
+        let sumProb = homeProb + awayProb
+        homeProb / sumProb
+//let rfl1819Data = Compact.deserializeFile<LeagueData> "RFL1819.json"
+//let rfl1718Data = Compact.deserializeFile<LeagueData> "RFL1718.json"
+//let rfl1617Data = Compact.deserializeFile<LeagueData> "RFL1617.json"
+//let rfl1516Data = Compact.deserializeFile<LeagueData> "RFL1516.json"
+//let rfl1415Data = Compact.deserializeFile<LeagueData> "RFL1415.json"
+//let rfl1314Data = Compact.deserializeFile<LeagueData> "RFL1314.json"
+//let rflMatches =
+//    [rfl1314Data.Matches; rfl1415Data.Matches; rfl1516Data.Matches;
+//    rfl1617Data.Matches; rfl1718Data.Matches; rfl1819Data.Matches]
+//    |> Array.concat
+//    |> Array.rev
+let checkEffectiveMarketHypothese fileNames =
+    let matches =
+        fileNames
+        |> List.map (fun fileName ->
+            let leagueData = Compact.deserializeFile<LeagueData> fileName
+            leagueData.Matches
+        )
+        |> Array.concat
+    printfn "Count %d" matches.Length
+
+    let mutable startingHomeSum = 0.f
+    let mutable closingHomeSum = 0.f
+    let mutable realHomeSum = 0.f
+    for m in matches do
+        //printfn "%A" m
+        let { Starting = starting; Closing = closing } = m.Odds
+        let result = getMatchResult m.Score
+        let realHome = if result = Home then 1.f else 0.f
+        let startingHomeProb = getProbabilities starting
+        let closingHomeProb = getProbabilities closing
+        realHomeSum <- realHomeSum + realHome
+        startingHomeSum <- startingHomeSum + startingHomeProb
+        closingHomeSum <- closingHomeSum + closingHomeProb
+        //printfn "Starting %f, Closing %f, Real %f" startingHomeProb closingHomeProb realHome
+        printfn "StartingSum %f, ClosingSum %f" (realHomeSum - startingHomeSum) (realHomeSum - closingHomeSum)
+        printfn ""
+
+// ("jytwvQhq", 5, "RFL1819.json"), ("hdM4QuuS", 5, "RFL1718.json"), ("dSBJYVTs", 5, "RFL1617.json"),
+// ("GQkWIAQ7", 5, "RFL1516.json"), ("Kh7n2gWp", 5, "RFL1415.json"), ("ITC1yoVJ", 5, "RFL1314.json")
+// ("fTANbXxl", 6, "OWL18.json")
+// ("xdttaT5s", 3, "LoLC19.json"), ("UqMHACKl", 8, "LoLC18.json"), ("tQQ0iGoN", 7, "LoLC17.json"), ("Ys6Qr462", 8, "LoLC16.json")
+// ("W2c1dRk0", 4, "LoLChina19.json"), ("OIF6g7Tp", 6, "LoLChina18.json"), ("hOlI07Jp", 5, "LoLChina17.json")
+
+let fetchLeagueDataAndSaveToFile sportID outID (leagueID, pageCount, fileName) =
+    let leagueRelativeUrl = "/ajax-sport-country-tournament-archive/" + sportID + "/" + leagueID + "/X0/1/0/"
+    let matches =
+        [1..pageCount]
+        |> List.map (fun pageNum ->
+            let leagueRelativeUrlNum = leagueRelativeUrl + pageNum.ToString() + "/"
+            let url = "https://fb.oddsportal.com" + leagueRelativeUrlNum + "?_=" + fromUnixTimestamp()
+            let content = fetchContent url "fb.oddsportal.com" "https://www.oddsportal.com/"
+            let json = extractDataFromResponse leagueRelativeUrlNum content |>> JsonValue.Parse
+            json |>> (fun value ->
+                let html = value?d?html.AsString()
+                let document = HtmlDocument()
+                document.LoadHtml(html)
+                extractMatches document |> List.map (extractOdds (sportID, outID, "1"))
+            ) |> defArg []
+        )
+        |> List.concat |> List.choose id |> List.toArray
+    let leagueData = { ID = leagueID; Matches = matches }
+    Compact.serializeToFile fileName leagueData
+
+
+// ("Uanezsbs", 30, "MLB19.json"), ("r3414Mwe", 58, "MLB18.json"), ("bwFloypH", 58, "MLB17.json")
 
 [<EntryPoint>]
 let main argv =
-    //jytwvQhq RFL1819
-    //hdM4QuuS RFL1718
-    //dSBJYVTs RFL1617
-    let url = "https://fb.oddsportal.com/ajax-sport-country-tournament-archive/1/jytwvQhq/X0/1/0/1/?_=" + fromUnixTimestamp()
-    let content = fetchContent url "fb.oddsportal.com" "https://www.oddsportal.com/"
-    let json = extractDataFromResponse "/ajax-sport-country-tournament-archive/1/jytwvQhq/X0/1/0/1/" content |>> JsonValue.Parse
-    let odds =
-        json |>> (fun value ->
-            let html = value?d?html.AsString()
-            let document = HtmlDocument()
-            document.LoadHtml(html)
-            let trs = document.DocumentNode.SelectNodes("/table/tbody/tr") |> List.ofSeq
-            let matches =
-                trs |> List.choose (fun tr ->
-                    let xeid = getAttribute tr (fun attr -> attr.Name = "xeid") |>> (fun attr -> attr.Value)
-                    let matchUrl = tr.ChildNodes |> List.ofSeq |> List.tryPick (fun child ->
-                        getAttribute child (fun attr -> attr.Name = "class" && attr.Value = "name table-participant") |>> (fun _ ->
-                            child.ChildNodes |> List.ofSeq |> List.tryFind (fun c -> c.Name = "a") |>> (fun a ->
-                                getAttribute a (fun attr -> attr.Name = "href") |>> (fun attr -> attr.Value) |> defArg ""
-                            ) |> defArg ""
-                        )
-                    )
-                    xeid |><| matchUrl
-                )
-            matches |> List.map (fun (matchID, matchRelativeUrl) ->
-                let matchUrl = "http://www.oddsportal.com/" + matchRelativeUrl
-                let xhash = parseMainMatchPage matchUrl
-                xhash |>> (fun (hash, score) ->
-                    let matchData = "/feed/match/1-1-" + matchID + "-1-2-" + hash + ".dat"
-                    let matchDataUrl = "https://fb.oddsportal.com" + matchData + "?_=" + fromUnixTimestamp()
-                    let matchContent = fetchContent matchDataUrl "fb.oddsportal.com" "https://www.oddsportal.com/"
-                    let pinnacleOdds = parseFootballMatchResponse matchData matchContent
-                    (pinnacleOdds, score)
-                )
-            )
-        )
+    fetchLeagueDataAndSaveToFile baseballID outHomeAwayID ("Uanezsbs", 30, "MLB19.json")
+    //checkEffectiveMarketHypothese ["MLB17.json"]
     0
