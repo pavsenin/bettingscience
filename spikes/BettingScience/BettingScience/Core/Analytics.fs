d171d18937f4c88234ce17e904d85912e8ffa4b6
pavsenin <pavsenin@mail.ru>
2019-07-16 11:01:22 +0300

    Next

--- spikes/BettingScience/BettingScience/Core/Analytics.fs
+++ spikes/BettingScience/BettingScience/Core/Analytics.fs
@@ -4,7 +4,7 @@ open Microsoft.FSharpLu.Json
 open Domain
 open Utils
 
-type MatchResult = Home | Draw | Away
+type MatchResult = O1 | O0 | O2
 type Accuracy = { Expected : float32; Variance : float32 }
 type AccuracyScoreX2 = { O1 : Accuracy; O2 : Accuracy }
 type AccuracyScoreX3 = { O1 : Accuracy; O0 : Accuracy; O2 : Accuracy }
@@ -15,33 +15,48 @@ let private getProbabilities value = function
     | X3 { O1 = (o1, _); O0 = (o0, _); O2 = (o2, _) } ->
         let o1Prob, o0Prob, o2Prob = 1.f / o1, 1.f / o0, 1.f / o2
         let sumProb = o1Prob + o0Prob + o2Prob
-        if value = Home then o1Prob / sumProb
-        else if value = Away then o2Prob / sumProb
+        if value = O1 then o1Prob / sumProb
+        else if value = O0 then o2Prob / sumProb
         else o0Prob / sumProb
     | X2 { O1 = (o1, _); O2 = (o2, _) } ->
         let o1Prob, o2Prob = 1.f / o1, 1.f / o2
         let sumProb = o1Prob + o2Prob
-        if value = Home then o1Prob / sumProb
+        if value = O1 then o1Prob / sumProb
         else o2Prob / sumProb
-let private getMatchResult ({ Home = home; Away = away }:MatchScore) = if home > away then Home else if home = away then Draw else Away
-let private getAccuracy out odds result { Expected = e; Variance = v } =
+let private getMatchResult ({ Home = home; Away = away }:MatchScore) = if home > away then O1 else if home = away then O0 else O2
+let private getMatchTotal ({ Home = home; Away = away }:MatchScore) = home + away
+let private getAccuracy out odds real { Expected = e; Variance = v } =
     let prob = getProbabilities out odds
-    let real = if result = out then 1.f else 0.f
     let diff = prob - real
     { Expected = e + diff; Variance = v + diff * diff }
-let compute state score odd =
+let compute state (score, ex) odd =
     match state with
     | { BookID = bookID; Opening = AX2 { O1 = oO1; O2 = oO2 }; Closing = AX2 { O1 = cO1; O2 = cO2 } } ->
-        match odd.Values with
-        | [|{ Value = None; BookOdds = bookOdds }|] ->
+        let value = odd.Values |> Array.tryFind (fun v -> ex = v.Value)
+        match value with
+        | Some { Value = None; BookOdds = bookOdds } ->
             let book = bookOdds |> Array.tryFind (fun b -> b.BookID = bookID)
             match book with
             | Some { Odds = { Opening = opening; Closing = closing } } ->
                 let result = getMatchResult score
-                let noO1 = getAccuracy Home opening result oO1
-                let noO2 = getAccuracy Away opening result oO2
-                let ncO1 = getAccuracy Home closing result cO1
-                let ncO2 = getAccuracy Away closing result cO2
+                let real01, real02 = if result = O1 then 1.f, 0.f else 0.f, 1.f
+                let noO1 = getAccuracy O1 opening real01 oO1
+                let noO2 = getAccuracy O2 opening real02 oO2
+                let ncO1 = getAccuracy O1 closing real01 cO1
+                let ncO2 = getAccuracy O2 closing real02 cO2
+
+                { BookID = bookID; Opening = AX2 { O1 = noO1; O2 = noO2 }; Closing = AX2 { O1 = ncO1; O2 = ncO2 } }
+            | _ -> state
+        | Some { Value = (Some h) as handicap; BookOdds = bookOdds } when handicap = ex ->
+            let book = bookOdds |> Array.tryFind (fun b -> b.BookID = bookID)
+            match book with
+            | Some { Odds = { Opening = opening; Closing = closing } } ->
+                let total = getMatchTotal score
+                let real01, real02 = if float32(total) > h then 1.f, 0.f else 0.f, 1.f
+                let noO1 = getAccuracy O1 opening real01 oO1
+                let noO2 = getAccuracy O2 opening real02 oO2
+                let ncO1 = getAccuracy O1 closing real01 cO1
+                let ncO2 = getAccuracy O2 closing real02 cO2
 
                 { BookID = bookID; Opening = AX2 { O1 = noO1; O2 = noO2 }; Closing = AX2 { O1 = ncO1; O2 = ncO2 } }
             | _ -> state
@@ -53,26 +68,30 @@ let compute state score odd =
             match book with
             | Some { Odds = { Opening = opening; Closing = closing } } ->
                 let result = getMatchResult score
-                let noO1 = getAccuracy Home opening result oO1
-                let noO0 = getAccuracy Draw opening result oO0
-                let noO2 = getAccuracy Away opening result oO2
-                let ncO1 = getAccuracy Home closing result cO1
-                let ncO0 = getAccuracy Draw closing result cO0
-                let ncO2 = getAccuracy Away closing result cO2
+                let real01, realO0, real02 =
+                    if result = O1 then 1.f, 0.f, 0.f
+                    else if result = O0 then 0.f, 1.f, 0.f
+                    else 0.f, 0.f, 1.f
+                let noO1 = getAccuracy O1 opening real01 oO1
+                let noO0 = getAccuracy O0 opening realO0 oO0
+                let noO2 = getAccuracy O2 opening real02 oO2
+                let ncO1 = getAccuracy O1 closing real01 cO1
+                let ncO0 = getAccuracy O0 closing realO0 cO0
+                let ncO2 = getAccuracy O2 closing real02 cO2
 
                 { BookID = bookID; Opening = AX3 { O1 = noO1; O0 = noO0; O2 = noO2 }; Closing = AX3 { O1 = ncO1; O0 = ncO0; O2 = ncO2 } }
             | _ -> state
         | _ -> state
     | _ -> state
 
-let analyze outID state fileName =
+let analyze (outID, ex) state fileName =
     let leagueData = Compact.deserializeFile<LeagueData> fileName
     let resultState =
         leagueData.Matches
         |> Array.fold (fun state matchData ->
             matchData.Odds
             |> Array.tryFind (fun out -> out.OutcomeID = outID)
-            |>> compute state matchData.Score
+            |>> compute state (matchData.Score, ex)
             |> defArg state
         ) state
     resultState
--- spikes/BettingScience/BettingScience/Program.fs
+++ spikes/BettingScience/BettingScience/Program.fs
@@ -7,9 +7,9 @@ open Analytics
 let main argv =
     // test crossed odds
     // test save/load data
-    // accuracy test from blocknote
+    // test accuracy 1x2, overunder, handicap
     // accuracy by total / handicap
-    // calculate accuracy by sports \ leagues \ seasons \ teams
+    // calculate accuracy by sports \ leagues \ seasons \ teams \ bookmakers \ outcomes 
     
     //[("C2416Q6r", 28, "NBA1819.json"); ("bwgTBOAd", 28, "NBA1718.json"); ("vBTNUyLS", 29, "NBA1617.json"); ("MmbLsWh8", 29, "NBA1516.json");
     // ("rcgzdfbO", 29, "NBA1415.json"); ("f7RlGfit", 29, "NBA1314.json"); ("0l4l9qpR", 29, "NBA1213.json"); ("lK6Chj6l", 23, "NBA1112.json")]
@@ -26,18 +26,28 @@ let main argv =
     //    OpeningScore = AX2 { O1 = { Expected = 0.f; Variance = 0.f }; O2 = { Expected = 0.f; Variance = 0.f } };
     //    ClosingScore = AX2 { O1 = { Expected = 0.f; Variance = 0.f }; O2 = { Expected = 0.f; Variance = 0.f } }
     //}
-    ["../../../data/soccer/rpl/RPL1819.json"; "../../../data/soccer/rpl/RPL1718.json"; "../../../data/soccer/rpl/RPL1617.json";
-     "../../../data/soccer/rpl/RPL1516.json"; "../../../data/soccer/rpl/RPL1415.json"; "../../../data/soccer/rpl/RPL1314.json"]
-    |> List.iter (fun file ->
-        [pinnacleID; betfairID; bet365ID; marafonID]
-        |> List.iter (fun bookID ->
-            let state = {
+    let getInitState out bookID =
+        if out = OddsPortalScraper.out1x2ID then
+            {
                 BookID = bookID;
                 Opening = AX3 { O1 = { Expected = 0.f; Variance = 0.f }; O0 = { Expected = 0.f; Variance = 0.f }; O2 = { Expected = 0.f; Variance = 0.f } };
                 Closing = AX3 { O1 = { Expected = 0.f; Variance = 0.f }; O0 = { Expected = 0.f; Variance = 0.f }; O2 = { Expected = 0.f; Variance = 0.f } }
             }
-            let result = analyze OddsPortalScraper.out1x2ID state file
-            printfn "%s %s %A" file bookID result
+        else
+            {
+                BookID = bookID;
+                Opening = AX2 { O1 = { Expected = 0.f; Variance = 0.f }; O2 = { Expected = 0.f; Variance = 0.f } };
+                Closing = AX2 { O1 = { Expected = 0.f; Variance = 0.f }; O2 = { Expected = 0.f; Variance = 0.f } }
+            }
+    ["../../../data/soccer/rpl/RPL1819.json"; "../../../data/soccer/rpl/RPL1718.json"; "../../../data/soccer/rpl/RPL1617.json";
+     "../../../data/soccer/rpl/RPL1516.json"; "../../../data/soccer/rpl/RPL1415.json"; "../../../data/soccer/rpl/RPL1314.json"]
+    |> List.iter (fun file ->
+        [(*OddsPortalScraper.out1x2ID;*) OddsPortalScraper.outOverUnderID]
+        |> List.iter (fun outID ->
+            let bookID = marafonID
+            let state = getInitState outID bookID
+            let result = analyze (outID, Some 0.5f) state file
+            printfn "%s %s %A" file outID result
         )
     )
     0
